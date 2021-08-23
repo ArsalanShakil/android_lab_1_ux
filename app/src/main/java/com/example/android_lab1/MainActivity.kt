2a35af22ea77a91f217b192b60871cf0a2619371
ArsalanShakil <arsalan_cr7@yahoo.com>
2021-08-23 14:07:50 +0300

    button click added

--- app/src/main/java/com/example/android_lab1/MainActivity.kt
+++ app/src/main/java/com/example/android_lab1/MainActivity.kt
@@ -2,15 +2,24 @@ package com.example.android_lab1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val greetingText = findViewById<TextView>(R.id.greetingText)
+        var count = 0
+        findViewById<Button>(R.id.clickBtn).setOnClickListener {
+            greetingText.text = "Goodbye Summer"
+            count++
+            if (count % 2 == 0) {
+                greetingText.text = "Hello World!"
+            }
 
-        greetingText.text = "hello worrrrld!"
+        }
 
 
     }
