a295bbc65fe7363a9825377e74bf5d2174a47d98
ArsalanShakil <arsalan_cr7@yahoo.com>
2021-08-24 09:49:46 +0300

    final version

--- app/src/main/java/com/example/android_lab1/MainActivity.kt
+++ app/src/main/java/com/example/android_lab1/MainActivity.kt
@@ -4,22 +4,61 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import android.widget.EditText
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val greetingText = findViewById<TextView>(R.id.greetingText)
-        var count = 0
         findViewById<Button>(R.id.clickBtn).setOnClickListener {
-            greetingText.text = "Goodbye Summer"
-            count++
-            if (count % 2 == 0) {
-                greetingText.text = "Hello World!"
+            val editTextPersonName = findViewById<EditText>(R.id.editTextPersonName)
+
+            val username: String = editTextPersonName.text.toString()
+
+            if (editTextPersonName.text.isNotEmpty()) {
+                val visibleTxt = "Hello $username"
+                greetingText.text = visibleTxt
+                editTextPersonName.text.clear()
+            } else {
+
+                when (greetingText.text) {
+                    "Hello World!" -> greetingText.text = "Goodbye Summer"
+                    "Goodbye Summer" -> greetingText.text = "Hello World!"
+                    else -> greetingText.text = "Hello World!"
+                }
+
             }
 
         }
+
+
+/*
+        clickBtn.setOnClickListener {
+            val username: String? = editTextPersonName.text.toString()
+
+            if (editTextPersonName.text.isEmpty()) {
+
+                when (greetingText.text) {
+                    "Hello World" -> greetingText.text = "Goodbye Summer"
+                    "Goodbye Summer" -> greetingText.text = "Hello World"
+                    else -> greetingText.text = "Hello World"
+                }
+
+            } else{
+                greetingText.text = "Hello $username "
+                editTextPersonName.text.clear()
+            }
+
+
+
+
+        }*/
+
+
 
 
     }
